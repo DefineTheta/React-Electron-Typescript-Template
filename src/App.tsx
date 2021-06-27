4678a6b56aaa21a5e3b68be9af8fea733ca96dc5
Theta <partheyb@gmail.com>
2021-06-27 16:57:54 -0700

    Added react-router-dom to the project (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Greetings from './components/Greetings';
 
@@ -11,9 +12,9 @@ document.body.appendChild(mainElement);
 
 const App = () => {
 	return (
-		<>
+		<Router>
 			<Greetings />
-		</>
+		</Router>
 	);
 };
 
